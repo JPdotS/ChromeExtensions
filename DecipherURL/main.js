40d4ea6c33fb798a2dad0e9c105d546697aa40b1
JayaPrakash Selvaraj <JPdotS@gmail.com>
2018-01-22 09:12:48 +0800

    reformat data

--- DecipherURL/main.js
+++ DecipherURL/main.js
@@ -2,9 +2,17 @@
 var url = window.location.toString();
 chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
   var element = document.querySelector("#greeting");
-    url = tabs[0].url;
-    var msg = "URL: ";
-    url = url.replace(/\//gi, "\n");
+
+    url = decodeURIComponent(tabs[0].url);
+
+    var msg = "The URL: \n";
+    msg = msg.concat(url)
+    msg = msg.concat("\n\n\nParts: \n")
+
+    // url = url.replace(/\/\//gi, "\/");
+    // url = url.replace(/\//gi, "\n");
+    url = url.replace(/\?/gi, "\n\? ");
+    url = url.replace(/\&/gi, "\n\& ");
     element.innerText = msg.concat(url);
 });
 
