95ed2ed5815e1cae34b25c6fdb85a5f0db45a3a3
jp <jayaprakash.selvaraj@astignes.com>
2022-06-16 11:58:50 +0800

    copy to clip

--- DecipherURL/main.js
+++ DecipherURL/main.js
@@ -1,18 +1,19 @@
 
 var url = window.location.toString();
-chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+  url = decodeURIComponent(tabs[0].url);
+  navigator.clipboard.writeText(url);
+
+  var msg = "The URL: \n";
+  msg = msg.concat(url)
+  msg = msg.concat("\n\n\nParts: \n")
+
+  // url = url.replace(/\/\//gi, "\/");
+  // url = url.replace(/\//gi, "\n");
+  url = url.replace(/\?/gi, "\n\? ");
+  url = url.replace(/\&/gi, "\n\& ");
+  
   var element = document.querySelector("#greeting");
-
-    url = decodeURIComponent(tabs[0].url);
-
-    var msg = "The URL: \n";
-    msg = msg.concat(url)
-    msg = msg.concat("\n\n\nParts: \n")
-
-    // url = url.replace(/\/\//gi, "\/");
-    // url = url.replace(/\//gi, "\n");
-    url = url.replace(/\?/gi, "\n\? ");
-    url = url.replace(/\&/gi, "\n\& ");
-    element.innerText = msg.concat(url);
+  element.innerText = msg.concat(url);
 });
 
