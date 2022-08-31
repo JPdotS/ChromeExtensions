922054ff7036001d03038170e98b8fab2a32f519
jp <jayaprakash.selvaraj@astignes.com>
2022-08-31 14:24:53 +0800

    more dev boxes

--- DuplicateTab/main.js
+++ DuplicateTab/main.js
@@ -8,17 +8,22 @@ chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
   // urlParts = /^(?:\w+\:\/\/)?([^\/]+)([^\?]*)\??(.*)$/.exec(url);
   // path = urlParts[2];
 
-  urlParts = url.split(domain)
-  path = urlParts[1]
+  var urlParts = url.split(domain)
+  var path = urlParts[1]
+
   if (path === '/'){
     path = ''
   }
 
-  proto_host = url.replace(path, '').toLowerCase()
+  var proto_host = url.replace(path, '').toLowerCase()
 
-  spaces =  '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'
+  // var spaces =  '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'
 
-  var _domain = domain
+  var _dev0_domain = 'adsg-ln21'
+  var _dev1_domain = 'cypnl01'
+  var _dev2_domain = 'forge'
+
+  var _domain = [_dev0_domain, _dev1_domain, _dev2_domain].includes(domain) ? 'cybox' : domain
   if (domain.endsWith('01') || domain.endsWith('02')) {
     _domain = domain.slice(0, -2)
   }
@@ -36,70 +41,57 @@ chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
     _qa_domain = 'qa'+_domain
   }
     
-  
-  var top = ' '
-  top += 'domain: <b>' + domain + '</b></br>'
+  var top = ''
+  top += '<div align="center"><b>' + domain + '</b> (this domain)</div>'
   top += '</br>'
   // top += 'proto_host: ' + proto_host + '</br>'
-  top += 'prod domain: ' + _domain + '</br>'
-  top += 'qa domain: ' + _qa_domain + '</br>'
-  top += '</br>'
-  top += 'path: ' + path + '</br>'
 
-  table =  ' '
+  var table =  ''
   table += '<table align="center">'
 
   table += '  <tr>'
   table += '    <td align="center">'
-  table += spaces
+  table += '<b><a href="'+ proto_host.replace(domain, _domain) + path +'" target="_blank"> '+ _domain +' </a></b>'
   table += '    </td>'
   table += '    <td align="center">'
-  table += spaces
+  table += '<b><a href="'+ proto_host.replace(domain, _qa_domain) + path +'" target="_blank"> '+ _qa_domain +' </a></b>'
   table += '    </td>'
   table += '    <td align="center">'
-  table += spaces
-  table += '    </td>'
-  table += '  </tr>'
-
-  table += '  <tr>'
-  table += '    <td align="center">'
-  table += '<b><a href="'+ proto_host.replace(domain, _domain) + path +'" target="_blank"> prod </a></b>'
-  table += '    </td>'
-  table += '    <td align="center">'
-  table += '<b><a href="'+ proto_host.replace(domain, _qa_domain) + path +'" target="_blank"> qa </a></b>'
-  table += '    </td>'
-  table += '    <td align="center">'
-  table += '<b><a href="'+ proto_host.replace(domain, 'adsg-ln21') + path +'" target="_blank"> dev </a></b>'
+  table += '<b><a href="'+ proto_host.replace(domain, _dev0_domain) + path +'" target="_blank"> '+ _dev0_domain +' </a></b>'
   table += '    </td>'
   table += '  </tr>'
 
   table += '  <tr>'
   table += '    <td align="center">'
-  table += '<a href="'+ proto_host.replace(domain, _domain+'01') + path +'" target="_blank">01</a>'
+  table += '<a href="'+ proto_host.replace(domain, _domain+'01') + path +'" target="_blank">'+_domain+'01'+'</a>'
   table += '    </td>'
   table += '    <td align="center">'
-  table += '<a href="'+ proto_host.replace(domain, _qa_domain+'01') + path +'" target="_blank">01</a>'
+  table += '<a href="'+ proto_host.replace(domain, _qa_domain+'01') + path +'" target="_blank">'+_qa_domain+'01'+'</a>'
   table += '    </td>'
   table += '    <td align="center">'
-  table += ''
+  table += '<b><a href="'+ proto_host.replace(domain, _dev1_domain) + path +'" target="_blank"> '+ _dev1_domain +' </a></b>'
   table += '    </td>'
   table += '  </tr>'
 
   table += '  <tr>'
   table += '    <td align="center">'
-  table += '<a href="'+ proto_host.replace(domain, _domain+'02') + path +'" target="_blank">02</a>'
+  table += '<a href="'+ proto_host.replace(domain, _domain+'02') + path +'" target="_blank">'+_domain+'02'+'</a>'
   table += '    </td>'
   table += '    <td align="center">'
-  table += '<a href="'+ proto_host.replace(domain, _qa_domain+'02') + path +'" target="_blank">02</a>'
+  table += '<a href="'+ proto_host.replace(domain, _qa_domain+'02') + path +'" target="_blank">'+_qa_domain+'02'+'</a>'
   table += '    </td>'
   table += '    <td align="center">'
-  table += ''
+  table += '<b><a href="'+ proto_host.replace(domain, _dev2_domain) + path +'" target="_blank"> '+ _dev2_domain +' </a></b>'
   table += '    </td>'
   table += '  </tr>'
   
   table += '</table>'
+
+  var bottom = ''
+  bottom += '</br>'
+  bottom += path + '  <i>(path)</i></br>'
   
   var element = document.querySelector("#greeting");
-  element.innerHTML = top + table;
+  element.innerHTML = top + table + bottom;
 });
 
